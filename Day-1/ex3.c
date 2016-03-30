b54e7262430f21e864069de47f0c33412ce5bddd
Casey Williams <williamshoops96@gmail.com>
2016-03-30 20:31:20 +0100

    finished ex 3

--- Day-1/ex3.c
+++ Day-1/ex3.c
@@ -4,8 +4,8 @@ int main()
 {
 	int age = 15;
 	int height = 72;
-	printf("I am %d years old.", age);
-	printf("I am %d inches tall.", height);
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
 
 	return 0;
 }
