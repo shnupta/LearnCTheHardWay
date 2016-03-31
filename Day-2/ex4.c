ff8416620ecc6e9b6be81a4a89ec465e01a3451e
Casey Williams <williamshoops96@gmail.com>
2016-03-31 10:39:59 +0100

    completed up to excersize 7

---
+++ Day-2/ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//purposely wrong to test the Valgrind tool
+
+int main() {
+
+	int age = 15;
+	int height;
+
+	printf("I am %d years old.\n" );
+	printf("I am %d inches tall.\n", height );
+
+	return 0;
+	
+}
